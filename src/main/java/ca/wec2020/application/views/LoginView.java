4209569e875085cc2741e515046501d326f1d747
evan-krul <evan@krul.ca>
2020-01-21 19:38:10 -0700

    Init setup

---
+++ src/main/java/ca/wec2020/application/views/LoginView.java
@@ -0,0 +1,55 @@
+package ca.wec2020.application.views;
+
+import ca.wec2020.application.app.security.CustomRequestCache;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collections;
+
+@Tag("sa-login-view")
+@Route(value = LoginView.ROUTE)
+@PageTitle("Login")
+public class LoginView extends VerticalLayout {
+	public static final String ROUTE = "login";
+
+	private LoginOverlay login = new LoginOverlay(); //
+
+	public LoginView(AuthenticationManager authenticationManager, //
+					 CustomRequestCache requestCache) {
+		// configures login dialog and adds it to the main view
+		login.setOpened(true);
+		login.setTitle("Spring Secured Vaadin");
+		login.setDescription("Login Overlay Example");
+
+		add(login);
+
+		login.addLoginListener(e -> { //
+			try {
+				// try to authenticate with given credentials, should always return not null or throw an {@link AuthenticationException}
+				final Authentication authentication = authenticationManager
+						.authenticate(new UsernamePasswordAuthenticationToken(e.getUsername(), e.getPassword())); //
+
+				// if authentication was successful we will update the security context and redirect to the page requested first
+				SecurityContextHolder.getContext().setAuthentication(authentication); //
+				login.close(); //
+				UI.getCurrent().navigate(requestCache.resolveRedirectUrl()); //
+
+			} catch (AuthenticationException ex) { //
+				// show default error message
+				// Note: You should not expose any detailed information here like "username is known but password is wrong"
+				// as it weakens security.
+				login.setError(true);
+			}
+		});
+	}
+}
---
+++ src/main/java/ca/wec2020/application/views/welcome/WelcomeView.java
@@ -0,0 +1,89 @@
+package ca.wec2020.application.views.welcome;
+
+import ca.wec2020.application.MainView;
+import ca.wec2020.application.views.WrapperCard;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.board.Board;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.springframework.security.access.annotation.Secured;
+
+@Route(value = "welcome", layout = MainView.class)
+@PageTitle("Welcome")
+@CssImport(value = "styles/views/style.css", include = "lumo-badge")
+@Secured("") //
+public class WelcomeView extends Div implements AfterNavigationObserver {
+    public WelcomeView() {
+        setId("welcome-view");
+
+        Board board = new Board();
+        board.addRow(
+                createBadge("Welcome", new H2("123"), "primary-text", "hello world", "badge")
+        );
+
+
+        // Create a chart of some primary type
+        Chart chart = new Chart(ChartType.SCATTER);
+
+// Modify the default configuration a bit
+        Configuration conf = chart.getConfiguration();
+        conf.setTitle("Average Temperatures in Turku");
+        conf.getLegend().setEnabled(false);
+
+// The primary data series
+        ListSeries averages = new ListSeries(
+                -6, -6.5, -4, 3, 9, 14, 17, 16, 11, 6, 2, -2.5);
+
+// Error bar data series with low and high values
+        DataSeries errors = new DataSeries();
+        errors.add(new DataSeriesItem(0,  -9, -3));
+        errors.add(new DataSeriesItem(1, -10, -3));
+        errors.add(new DataSeriesItem(2,  -8,  1));
+
+// Need to be used for series to be recognized as error bar
+        PlotOptionsErrorbar barOptions = new PlotOptionsErrorbar();
+        errors.setPlotOptions(barOptions);
+
+// The errors should be drawn lower
+        conf.addSeries(errors);
+        conf.addSeries(averages);
+
+        WrapperCard welcomeWrapper = new WrapperCard("wrapper",
+                new Component[] { chart }, "card");
+        board.add(welcomeWrapper);
+//        board.add(chart);
+
+
+        add(board);
+    }
+
+    @Override
+    public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
+
+    }
+
+    private WrapperCard createBadge(String title, H2 h2, String h2ClassName,
+                                    String description, String badgeTheme) {
+        Span titleSpan = new Span(title);
+        titleSpan.getElement().setAttribute("theme", badgeTheme);
+
+        h2.addClassName(h2ClassName);
+
+        Span descriptionSpan = new Span(description);
+        descriptionSpan.addClassName("secondary-text");
+
+        return new WrapperCard("wrapper",
+                new Component[] { titleSpan, h2, descriptionSpan }, "card",
+                "space-m");
+    }
+
+}
